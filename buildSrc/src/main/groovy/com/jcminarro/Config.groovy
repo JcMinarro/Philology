4d6e198828a4eb7047c3934b65b07c9cf2bd93d7
Jc Miñarro <josecarlos.minarro@gmail.com>
2018-07-03 00:03:47 +0200

    Add deploy script

--- buildSrc/src/main/groovy/com/jcminarro/Config.groovy
+++ buildSrc/src/main/groovy/com/jcminarro/Config.groovy
@@ -6,4 +6,18 @@ class Config {
     static final int minSdkVersion = 16
     static final int versionCode = 1
     static final String versionName = "1.0.0"
+    static final String group = "com.jcminarro"
+    static final String pomArtifactId = "Philology"
+    static final String pomName = "Philology"
+    static final String pomDescription = "Reword Android Strings resources dynamically"
+    static final String pomURL = "https://github.com/JcMinarro/Philology"
+    static final String pomSCMURL = "https://github.com/JcMinarro/Philology"
+    static final String pomSCMConnection = "scm:git@github.com/JcMinarro/Philology.git"
+    static final String pomSCMCDevonnection = "scm:git@github.com/JcMinarro/Philology.git"
+    static final String pomLicenceName = "The Apache Software License, Version 2.0"
+    static final String pomLicenceUrl = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+    static final String pomLicenceDist = "repo"
+    static final String pomDeveloperId = "JcMinarro"
+    static final String pomDeveloperName = "Jc Miñarro"
+    static final String pomDeveloperEmail = "josecarlos.minarro@gmail.com"
 }
