a30a64d65acba1e0e1ee4282d8fc4f9a82829242
Jc Miñarro <josecarlos.minarro@gmail.com>
2018-08-05 00:49:09 +0200

    Fix crash when check if view is Toolbar in Android version lower than Lolipop (#3)
    * Fix crash when check if view is Toolbar in Android version lower than Lolipop
    
    * Fix PhilologyTests

--- philology/src/test/java/com/jcminarro/philology/PhilologyTest.kt
+++ philology/src/test/java/com/jcminarro/philology/PhilologyTest.kt
@@ -1,5 +1,16 @@
 package com.jcminarro.philology
 
+import android.os.Build.VERSION_CODES.JELLY_BEAN
+import android.os.Build.VERSION_CODES.JELLY_BEAN_MR1
+import android.os.Build.VERSION_CODES.JELLY_BEAN_MR2
+import android.os.Build.VERSION_CODES.KITKAT
+import android.os.Build.VERSION_CODES.LOLLIPOP
+import android.os.Build.VERSION_CODES.LOLLIPOP_MR1
+import android.os.Build.VERSION_CODES.M
+import android.os.Build.VERSION_CODES.N
+import android.os.Build.VERSION_CODES.N_MR1
+import android.os.Build.VERSION_CODES.O
+import android.os.Build.VERSION_CODES.O_MR1
 import android.support.v7.widget.Toolbar
 import android.widget.TextView
 import com.jcminarro.philology.transformer.NoneViewTransformer
@@ -13,8 +24,10 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.RuntimeEnvironment.application
+import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
+@Config(sdk = [JELLY_BEAN, JELLY_BEAN_MR1, JELLY_BEAN_MR2, KITKAT, LOLLIPOP, LOLLIPOP_MR1, M, N, N_MR1, O, O_MR1])
 class PhilologyTest {
 
     @Test
@@ -33,8 +46,15 @@ class PhilologyTest {
     }
 
     @Test
+    @Config(sdk = [LOLLIPOP, LOLLIPOP_MR1, M, N, N_MR1, O, O_MR1])
     fun `Should provide a ToolbarViewTransformer`() {
         Philology.getViewTransformer(mock<android.widget.Toolbar>()) `should be` ToolbarViewTransformer
+    }
+
+    @Test
+    @Config(sdk = [JELLY_BEAN, JELLY_BEAN_MR1, JELLY_BEAN_MR2, KITKAT])
+    fun `Shouldn't provide a ToolbarViewTransformer because Toolbar doesn't exist on those APIs`() {
+        Philology.getViewTransformer(mock<android.widget.Toolbar>()) `should be` NoneViewTransformer
     }
 
     @Test
