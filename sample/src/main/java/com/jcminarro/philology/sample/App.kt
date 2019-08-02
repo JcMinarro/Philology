8bd30d4bef5c33d64f68ddf160dca99f6416a3ee
Serghei Oleinicenco <serghei.oleinicenco@ellation.com>
2019-08-02 18:16:26 +0300

    Update sample app with russian locale

--- sample/src/main/java/com/jcminarro/philology/sample/App.kt
+++ sample/src/main/java/com/jcminarro/philology/sample/App.kt
@@ -24,9 +24,10 @@ class App : Application() {
 }
 
 object MyPhilologyRepositoryFactory : PhilologyRepositoryFactory {
-    override fun getPhilologyRepository(locale: Locale): PhilologyRepository? = when {
-        Locale.ENGLISH.language == locale.language -> EnglishPhilologyRepository
-        Locale("es", "ES").language == locale.language -> SpanishPhilologyRepository
+    override fun getPhilologyRepository(locale: Locale): PhilologyRepository? = when (locale) {
+        Locale.ENGLISH -> EnglishPhilologyRepository
+        Locale("es", "ES") -> SpanishPhilologyRepository
+        Locale("ru", "RU") -> RussianPhilologyRepository
 // If we don't support a language we could return null as PhilologyRepository and
 // values from the strings resources file will be used
         else -> null
@@ -35,7 +36,7 @@ object MyPhilologyRepositoryFactory : PhilologyRepositoryFactory {
 
 object EnglishPhilologyRepository : PhilologyRepository {
     private val pluralsSample = mapOf("one" to "test one", "other" to "test other")
-    private val pluralsSampleFormat = mapOf("one" to "test one %s", "other" to "test other %s")
+    private val pluralsSampleFormat = mapOf("one" to "%s test one", "other" to "%s tests")
     private val resourceSample = mapOf(
         "plurals_sample" to pluralsSample,
         "plurals_sample_format" to pluralsSampleFormat
@@ -66,7 +67,34 @@ object SpanishPhilologyRepository : PhilologyRepository {
             else -> null
         }
         is Plural -> resourceSample[resource.key]?.get(resource.quantityKeyword)
-// If we don't want reword an strings we could return null and the value from the string resources file will be used
+        else -> null
+    }
+}
+
+object RussianPhilologyRepository : PhilologyRepository {
+    private val pluralsSample = mapOf(
+        "one" to "один тест",
+        "other" to "другие тесты",
+        "few" to "несколько теста",
+        "many" to "много тестов"
+    )
+    private val pluralsSampleFormat = mapOf(
+        "one" to "%s тест",
+        "other" to "%s тесты",
+        "few" to "%s теста",
+        "many" to "%s тестов"
+    )
+    private val resourceSample = mapOf(
+        "plurals_sample" to pluralsSample,
+        "plurals_sample_format" to pluralsSampleFormat
+    )
+
+    override fun getText(resource: Resource): CharSequence? = when (resource) {
+        is Text -> when (resource.key) {
+            "label" -> "Новое значение для ключа `label`, его можно получить из базы данных или внешнего сервера API"
+            else -> null
+        }
+        is Plural -> resourceSample[resource.key]?.get(resource.quantityKeyword)
         else -> null
     }
 }
