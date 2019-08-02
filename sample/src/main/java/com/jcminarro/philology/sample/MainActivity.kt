99a9d5d06ab7cab5541f50c984858de0945f83cb
Serghei Oleinicenco <serghei.oleinicenco@ellation.com>
2019-08-02 18:17:52 +0300

    Update sample app with editable quantity fields

--- sample/src/main/java/com/jcminarro/philology/sample/MainActivity.kt
+++ sample/src/main/java/com/jcminarro/philology/sample/MainActivity.kt
@@ -3,6 +3,9 @@ package com.jcminarro.philology.sample
 import android.content.Context
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
 import android.widget.TextView
 import com.jcminarro.philology.Philology
 import com.jcminarro.sample.R
@@ -17,9 +20,32 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<TextView>(R.id.plural_label).text =
-            resources.getQuantityString(R.plurals.plurals_sample, 1)
-        findViewById<TextView>(R.id.plural_format_label).text =
-            resources.getQuantityString(R.plurals.plurals_sample_format, 1, "or another")
+        val pluralLabel = findViewById<TextView>(R.id.plural_label)
+        findViewById<EditText>(R.id.plural_quantity_edit).afterTextChanged { text: String ->
+            text.toIntOrNull()?.let {
+                pluralLabel.text = resources.getQuantityString(R.plurals.plurals_sample, it)
+            }
+        }
+
+        val pluralFormatLabel = findViewById<TextView>(R.id.plural_format_label)
+        findViewById<EditText>(R.id.plural_quantity_format_edit).afterTextChanged { text: String ->
+            text.toIntOrNull()?.let {
+                pluralFormatLabel.text = resources.getQuantityString(R.plurals.plurals_sample_format, it, it)
+            }
+        }
+    }
+
+    private fun EditText.afterTextChanged(afterTextChanged: (String) -> Unit) {
+        this.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun afterTextChanged(editable: Editable?) {
+                afterTextChanged.invoke(editable.toString())
+            }
+        })
     }
 }
