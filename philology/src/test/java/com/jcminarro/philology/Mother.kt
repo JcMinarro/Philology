e29d369f36aaf3792155a6b943e35853d0566acf
Ilya Ghirici <ilya.ghirici@gmail.com>
2019-11-28 18:09:42 +0200

    Cover TextViewTransformer with unit tests

--- philology/src/test/java/com/jcminarro/philology/Mother.kt
+++ philology/src/test/java/com/jcminarro/philology/Mother.kt
@@ -1,17 +1,20 @@
 package com.jcminarro.philology
 
+import android.content.Context
 import android.content.res.Configuration
 import android.content.res.Resources
+import android.content.res.TypedArray
 import android.os.LocaleList
 import android.util.AttributeSet
+import com.nhaarman.mockito_kotlin.any
 import com.nhaarman.mockito_kotlin.doAnswer
 import com.nhaarman.mockito_kotlin.doReturn
 import com.nhaarman.mockito_kotlin.doThrow
 import com.nhaarman.mockito_kotlin.eq
+import com.nhaarman.mockito_kotlin.whenever
 import org.amshove.kluent.When
 import org.amshove.kluent.calling
 import org.amshove.kluent.mock
-import org.mockito.Mockito
 import java.util.Locale
 
 fun createConfiguration(locale: Locale = Locale.ENGLISH): Configuration = mock<Configuration>().apply {
@@ -87,23 +90,37 @@ fun configurePhilology(repository: PhilologyRepository, locale: Locale = Locale.
     Philology.init(createFactory(locale to repository))
 }
 
-fun createAttributeSet(vararg attributeType: AttributeType): AttributeSet = mock<AttributeSet>().apply {
+fun createAttributeSet(
+    vararg attributeType: AttributeType,
+    context: Context = mock()
+): AttributeSet = mock<AttributeSet>().apply {
     attributeType.forEachIndexed { index, at ->
         when (at) {
             is HardcodedAttribute -> {
-                Mockito.`when`(this.getAttributeResourceValue(eq(index), org.amshove.kluent.any()))
+                whenever(this.getAttributeResourceValue(eq(index), any()))
                     .doAnswer { it.arguments[1] as Int }
             }
             is ResourceIdAttribute -> {
-                When calling this.getAttributeResourceValue(
+                When calling this.getAttributeResourceValue(eq(index), any()) doReturn index
+            }
+            is StyleAttribute -> {
+                val styleAttributes = createStyledAttributes(at.styleAttributes)
+                When calling context.obtainStyledAttributes(
                     eq(index),
-                    org.amshove.kluent.any()
-                ) doReturn index
+                    any()
+                ) doReturn styleAttributes
+                When calling this.getAttributeResourceValue(eq(index), any()) doReturn index
             }
         }
         When calling this.getAttributeName(index) doReturn at.key
     }
     When calling this.attributeCount doReturn attributeType.size
+}
+
+fun createStyledAttributes(styleAttributes: IntArray): TypedArray = mock<TypedArray>().apply {
+    styleAttributes.forEachIndexed { index, _ ->
+        When calling this.getResourceId(eq(index), any()) doReturn index
+    }
 }
 
 sealed class AttributeType {
@@ -112,3 +129,7 @@ sealed class AttributeType {
 
 data class HardcodedAttribute(override val key: String) : AttributeType()
 data class ResourceIdAttribute(override val key: String) : AttributeType()
+data class StyleAttribute(
+    val styleAttributes: IntArray,
+    override val key: String = "style"
+) : AttributeType()
--- philology/src/test/java/com/jcminarro/philology/transformer/TextViewTransformerTest.kt
+++ philology/src/test/java/com/jcminarro/philology/transformer/TextViewTransformerTest.kt
@@ -1,14 +1,19 @@
 package com.jcminarro.philology.transformer
 
+import android.content.Context
 import android.view.View
 import android.widget.TextView
 import com.jcminarro.philology.HardcodedAttribute
 import com.jcminarro.philology.ResourceIdAttribute
+import com.jcminarro.philology.StyleAttribute
 import com.jcminarro.philology.createAttributeSet
+import com.nhaarman.mockito_kotlin.doReturn
 import org.amshove.kluent.Verify
+import org.amshove.kluent.When
 import org.amshove.kluent.`Verify no further interactions`
 import org.amshove.kluent.`should be`
 import org.amshove.kluent.called
+import org.amshove.kluent.calling
 import org.amshove.kluent.mock
 import org.amshove.kluent.on
 import org.amshove.kluent.that
@@ -19,6 +24,7 @@ class TextViewTransformerTest {
 
     private val view: View = mock()
     private val textView: TextView = mock()
+    private val context: Context = mock()
 
     @Test
     fun `View should be the same`() {
@@ -39,9 +45,12 @@ class TextViewTransformerTest {
 
     @Test
     fun `Should reword only the text`() {
-        val viewResult = TextViewTransformer.reword(textView, createAttributeSet(
+        val viewResult = TextViewTransformer.reword(
+            textView, createAttributeSet(
                 HardcodedAttribute("hint"),
-                ResourceIdAttribute("text")))
+                ResourceIdAttribute("text")
+            )
+        )
 
         viewResult `should be` textView
         Verify on textView that textView.setText(1) was called
@@ -50,9 +59,12 @@ class TextViewTransformerTest {
 
     @Test
     fun `Should reword only the hint`() {
-        val viewResult = TextViewTransformer.reword(textView, createAttributeSet(
+        val viewResult = TextViewTransformer.reword(
+            textView, createAttributeSet(
                 HardcodedAttribute("text"),
-                ResourceIdAttribute("hint")))
+                ResourceIdAttribute("hint")
+            )
+        )
 
         viewResult `should be` textView
         Verify on textView that textView.setHint(1) was called
@@ -61,13 +73,59 @@ class TextViewTransformerTest {
 
     @Test
     fun `Should reword text and hint`() {
-        val viewResult = TextViewTransformer.reword(textView, createAttributeSet(
+        val viewResult = TextViewTransformer.reword(
+            textView, createAttributeSet(
                 ResourceIdAttribute("text"),
-                ResourceIdAttribute("hint")))
+                ResourceIdAttribute("hint")
+            )
+        )
 
         viewResult `should be` textView
         Verify on textView that textView.setText(0) was called
         Verify on textView that textView.setHint(1) was called
         `Verify no further interactions` on textView
+    }
+
+    @Test
+    fun `Should reword only text from style`() {
+        When calling textView.context doReturn context
+
+        val viewResult = TextViewTransformer.reword(
+            textView,
+            createAttributeSet(StyleAttribute(intArrayOf(android.R.attr.text)), context = context)
+        )
+
+        viewResult `should be` textView
+        Verify on textView that textView.setText(0) was called
+    }
+
+    @Test
+    fun `Should reword only hint from style`() {
+        When calling textView.context doReturn context
+
+        val viewResult = TextViewTransformer.reword(
+            textView,
+            createAttributeSet(StyleAttribute(intArrayOf(android.R.attr.hint)), context = context)
+        )
+
+        viewResult `should be` textView
+        Verify on textView that textView.setHint(0) was called
+    }
+
+    @Test
+    fun `Should reword text and hint from style`() {
+        When calling textView.context doReturn context
+
+        val viewResult = TextViewTransformer.reword(
+            textView,
+            createAttributeSet(
+                StyleAttribute(intArrayOf(android.R.attr.text, android.R.attr.hint)),
+                context = context
+            )
+        )
+
+        viewResult `should be` textView
+        Verify on textView that textView.setText(0) was called
+        Verify on textView that textView.setHint(1) was called
     }
 }
