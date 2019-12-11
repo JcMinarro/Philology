a304b1adbab0eb4c8dcf5b294d4821b992145c64
Valeriu Turcan <valeriu.turcan@ellation.com>
2019-12-11 14:50:22 +0200

    Simplify and improve text resId read logic from AttributeSet

--- philology/src/main/java/com/jcminarro/philology/ViewTransformer.kt
+++ philology/src/main/java/com/jcminarro/philology/ViewTransformer.kt
@@ -3,51 +3,20 @@ package com.jcminarro.philology
 import android.content.Context
 import android.util.AttributeSet
 import android.view.View
+import androidx.annotation.AttrRes
+import androidx.annotation.StringRes
 
 interface ViewTransformer {
     fun reword(view: View, attributeSet: AttributeSet): View
 
-    fun setTextIfExists(attributeSet: AttributeSet, index: Int, setTextResAction: (Int) -> Unit) {
-        attributeSet.getAttributeResourceValue(index, -1).takeIf { it != -1 }
-            ?.let { setTextResAction(it) }
-    }
-
-    fun setTextIfExists(
-        context: Context,
-        textIndex: Int?,
-        styleIndex: Int?,
+    @StringRes
+    fun Context.getStringResourceId(
         attributeSet: AttributeSet,
-        attributeFromStyle: Int,
-        setTextResAction: (Int) -> Unit
-    ) {
-        if (textIndex != null) {
-            setTextIfExists(attributeSet, textIndex, setTextResAction)
-        } else styleIndex?.let {
-            setTextIfExistsInStyle(
-                context = context,
-                attributeSet = attributeSet,
-                styleIndex = styleIndex,
-                attributeFromStyle = attributeFromStyle,
-                setTextResAction = setTextResAction
-            )
-        }
-    }
-
-    private fun setTextIfExistsInStyle(
-        context: Context,
-        attributeSet: AttributeSet,
-        styleIndex: Int,
-        attributeFromStyle: Int,
-        setTextResAction: (Int) -> Unit
-    ) {
-        val styleResource =
-            attributeSet.getAttributeResourceValue(styleIndex, -1).takeIf { it != -1 }
-        styleResource?.let {
-            val styleAttr =
-                context.obtainStyledAttributes(styleResource, intArrayOf(attributeFromStyle))
-            val textResource = styleAttr.getResourceId(0, -1).takeIf { it != -1 }
-            textResource?.let(setTextResAction)
-            styleAttr.recycle()
-        }
+        @AttrRes attrResId: Int
+    ): Int {
+        val typedArray = obtainStyledAttributes(attributeSet, intArrayOf(attrResId))
+        val stringResId = typedArray.getResourceId(0, -1)
+        typedArray.recycle()
+        return stringResId
     }
 }
