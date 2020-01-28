5eea6a39eacdd975169ab036dadf27281ae28204
Serghei Ceacal <serghei.ceacal@ellation.com>
2020-01-28 17:12:05 +0200

    Override all methods in philology resources (#29)
    * Use base Resources to get non-text resources in PhilologyResources
    
    * Update layout of Main Activity to make content scrollable
    
    * Do not override updateConfiguration to fix crash on Android 6.0
    
    * Convert concatenation to template in CustomView
    
    * Fix lint violation for getFont(Int) method in PhilologyResources
    
    * Use expression body style for all get methods in PhilologyResources
    
    * Keep @Throws annotations for overriden methods in PhilologyResources
    
    * Cover all methods in PhilologyResources with tests

--- philology/src/main/java/com/jcminarro/philology/PhilologyResources.kt
+++ philology/src/main/java/com/jcminarro/philology/PhilologyResources.kt
@@ -1,10 +1,30 @@
 package com.jcminarro.philology
 
+import android.annotation.SuppressLint
+import android.annotation.TargetApi
+import android.content.res.AssetFileDescriptor
+import android.content.res.ColorStateList
+import android.content.res.Configuration
 import android.content.res.Resources
+import android.content.res.TypedArray
+import android.content.res.XmlResourceParser
+import android.graphics.Movie
+import android.graphics.Typeface
+import android.graphics.drawable.Drawable
+import android.os.Build.VERSION_CODES
+import android.os.Bundle
+import android.util.AttributeSet
+import android.util.DisplayMetrics
+import android.util.TypedValue
+import androidx.annotation.RequiresApi
+import org.xmlpull.v1.XmlPullParserException
+import java.io.IOException
+import java.io.InputStream
 
 @Suppress("DEPRECATION")
-internal class PhilologyResources(baseResources: Resources) :
-    Resources(baseResources.assets, baseResources.displayMetrics, baseResources.configuration) {
+internal class PhilologyResources(
+    private val baseResources: Resources
+) : Resources(baseResources.assets, baseResources.displayMetrics, baseResources.configuration) {
     private val resourcesUtil = ResourcesUtil(baseResources)
 
     override fun getText(id: Int): CharSequence = resourcesUtil.getText(id)
@@ -29,4 +49,138 @@ internal class PhilologyResources(baseResources: Resources) :
     override fun getStringArray(id: Int): Array<String> = resourcesUtil.getStringArray(id)
 
     override fun getTextArray(id: Int): Array<CharSequence> = resourcesUtil.getTextArray(id)
+
+    @Throws(NotFoundException::class)
+    override fun getAnimation(id: Int): XmlResourceParser = baseResources.getAnimation(id)
+
+    override fun getDisplayMetrics(): DisplayMetrics = baseResources.displayMetrics
+
+    override fun getDrawableForDensity(id: Int, density: Int): Drawable? =
+        baseResources.getDrawableForDensity(id, density)
+
+    @TargetApi(VERSION_CODES.LOLLIPOP)
+    override fun getDrawableForDensity(id: Int, density: Int, theme: Theme?): Drawable? =
+        baseResources.getDrawableForDensity(id, density, theme)
+
+    override fun getConfiguration(): Configuration = baseResources.configuration
+
+    override fun obtainAttributes(set: AttributeSet?, attrs: IntArray?): TypedArray {
+        return baseResources.obtainAttributes(set, attrs)
+    }
+
+    @Throws(NotFoundException::class)
+    override fun getDimensionPixelSize(id: Int): Int = baseResources.getDimensionPixelSize(id)
+
+    @Throws(NotFoundException::class)
+    override fun getIntArray(id: Int): IntArray = baseResources.getIntArray(id)
+
+    @Throws(NotFoundException::class)
+    override fun getValue(id: Int, outValue: TypedValue?, resolveRefs: Boolean) {
+        baseResources.getValue(id, outValue, resolveRefs)
+    }
+
+    @Throws(NotFoundException::class)
+    override fun getValue(name: String?, outValue: TypedValue?, resolveRefs: Boolean) {
+        baseResources.getValue(name, outValue, resolveRefs)
+    }
+
+    @Throws(NotFoundException::class)
+    override fun getResourcePackageName(resid: Int): String =
+        baseResources.getResourcePackageName(resid)
+
+    @Throws(NotFoundException::class)
+    override fun openRawResourceFd(id: Int): AssetFileDescriptor =
+        baseResources.openRawResourceFd(id)
+
+    @Throws(NotFoundException::class)
+    override fun getDimension(id: Int): Float = baseResources.getDimension(id)
+
+    @Throws(NotFoundException::class)
+    override fun getColorStateList(id: Int): ColorStateList = baseResources.getColorStateList(id)
+
+    @TargetApi(VERSION_CODES.M)
+    @Throws(NotFoundException::class)
+    override fun getColorStateList(id: Int, theme: Theme?): ColorStateList =
+        baseResources.getColorStateList(id, theme)
+
+    @Throws(NotFoundException::class)
+    override fun getBoolean(id: Int): Boolean = baseResources.getBoolean(id)
+
+    override fun getIdentifier(name: String?, defType: String?, defPackage: String?): Int =
+        baseResources.getIdentifier(name, defType, defPackage)
+
+    @Throws(NotFoundException::class)
+    override fun getColor(id: Int): Int = baseResources.getColor(id)
+
+    @TargetApi(VERSION_CODES.M)
+    @Throws(NotFoundException::class)
+    override fun getColor(id: Int, theme: Theme?): Int = baseResources.getColor(id, theme)
+
+    override fun openRawResource(id: Int): InputStream = baseResources.openRawResource(id)
+
+    @Throws(NotFoundException::class)
+    override fun openRawResource(id: Int, value: TypedValue?): InputStream =
+        baseResources.openRawResource(id, value)
+
+    @Throws(NotFoundException::class)
+    override fun getMovie(id: Int): Movie = baseResources.getMovie(id)
+
+    @Throws(NotFoundException::class)
+    override fun getInteger(id: Int): Int = baseResources.getInteger(id)
+
+    @Throws(XmlPullParserException::class, IOException::class)
+    override fun parseBundleExtras(parser: XmlResourceParser?, outBundle: Bundle?) {
+        this.baseResources.parseBundleExtras(parser, outBundle)
+    }
+
+    @Throws(NotFoundException::class)
+    override fun getDrawable(id: Int): Drawable = baseResources.getDrawable(id)
+
+    @TargetApi(VERSION_CODES.LOLLIPOP)
+    @Throws(NotFoundException::class)
+    override fun getDrawable(id: Int, theme: Theme?): Drawable =
+        baseResources.getDrawable(id, theme)
+
+    @Throws(NotFoundException::class)
+    override fun getResourceTypeName(resid: Int): String = baseResources.getResourceTypeName(resid)
+
+    @Throws(NotFoundException::class)
+    override fun getLayout(id: Int): XmlResourceParser = baseResources.getLayout(id)
+
+    @SuppressLint("NewApi")
+    @RequiresApi(VERSION_CODES.O)
+    @Throws(NotFoundException::class)
+    override fun getFont(id: Int): Typeface = baseResources.getFont(id)
+
+    @Throws(NotFoundException::class)
+    override fun getXml(id: Int): XmlResourceParser = baseResources.getXml(id)
+
+    @Throws(NotFoundException::class)
+    override fun getResourceName(resid: Int): String = baseResources.getResourceName(resid)
+
+    @Throws(XmlPullParserException::class)
+    override fun parseBundleExtra(tagName: String?, attrs: AttributeSet?, outBundle: Bundle?) {
+        baseResources.parseBundleExtra(tagName, attrs, outBundle)
+    }
+
+    @Throws(NotFoundException::class)
+    override fun getDimensionPixelOffset(id: Int): Int = baseResources.getDimensionPixelOffset(id)
+
+    @Throws(NotFoundException::class)
+    override fun getValueForDensity(
+        id: Int,
+        density: Int,
+        outValue: TypedValue?,
+        resolveRefs: Boolean
+    ) {
+        baseResources.getValueForDensity(id, density, outValue, resolveRefs)
+    }
+
+    @Throws(NotFoundException::class)
+    override fun getResourceEntryName(resid: Int): String =
+        baseResources.getResourceEntryName(resid)
+
+    @Throws(NotFoundException::class)
+    override fun getFraction(id: Int, base: Int, pbase: Int): Float =
+        baseResources.getFraction(id, base, pbase)
 }
--- philology/src/test/java/com/jcminarro/philology/PhilologyResourcesTest.kt
+++ philology/src/test/java/com/jcminarro/philology/PhilologyResourcesTest.kt
@@ -1,14 +1,28 @@
 package com.jcminarro.philology
 
+import android.content.res.AssetFileDescriptor
+import android.content.res.ColorStateList
 import android.content.res.Configuration
 import android.content.res.Resources
+import android.content.res.Resources.Theme
+import android.content.res.TypedArray
+import android.content.res.XmlResourceParser
+import android.graphics.Movie
+import android.graphics.Typeface
+import android.graphics.drawable.Drawable
+import android.os.Bundle
+import android.util.AttributeSet
+import android.util.DisplayMetrics
+import android.util.TypedValue
 import com.nhaarman.mockito_kotlin.doReturn
+import com.nhaarman.mockito_kotlin.verify
 import org.amshove.kluent.When
 import org.amshove.kluent.`should equal`
 import org.amshove.kluent.calling
 import org.amshove.kluent.mock
 import org.junit.Before
 import org.junit.Test
+import java.io.InputStream
 
 class PhilologyResourcesTest {
 
@@ -118,14 +132,14 @@ class PhilologyResourcesTest {
         resources.getTextArray(id) `should equal` textArray
     }
 
-    @Test()
+    @Test
     fun `Should return a CharSequence from repository asking for a text`() {
         configureResourceGetText(baseResources, id, nameId, someCharSequence)
         configurePhilology(createRepository(nameId, null, repoCharSequence))
         resources.getText(id) `should equal` repoCharSequence
     }
 
-    @Test()
+    @Test
     fun `Should return a CharSequence from repository asking for an String`() {
         configureResourceGetText(baseResources, id, nameId, someCharSequence)
         configurePhilology(createRepository(nameId, null, repoCharSequence))
@@ -162,5 +176,260 @@ class PhilologyResourcesTest {
         configureResourceGetTextArray(baseResources, id, nameId, textArray)
         configurePhilology(createRepository(nameId, textArray = textArray))
         resources.getTextArray(id) `should equal` textArray
+    }
+
+    @Test
+    fun `Should return an XmlResourceParser from base resources asking for an Animation`() {
+        val xmlResourceParser = mock<XmlResourceParser>()
+        When calling baseResources.getAnimation(id) doReturn xmlResourceParser
+        resources.getAnimation(id) `should equal` xmlResourceParser
+    }
+
+    @Test
+    fun `Should return a DisplayMetrics from base resources asking for a display metrics`() {
+        val displayMetrics = mock<DisplayMetrics>()
+        When calling baseResources.displayMetrics doReturn displayMetrics
+        resources.displayMetrics `should equal` displayMetrics
+    }
+
+    @Test
+    fun `Should return a Drawable from base resources asking for a drawable for density`() {
+        val drawable = mock<Drawable>()
+        When calling baseResources.getDrawableForDensity(id, 2) doReturn drawable
+        resources.getDrawableForDensity(id, 2) `should equal` drawable
+    }
+
+    @Test
+    fun `Should return a Drawable from base resources asking for a drawable for density on API 21+`() {
+        val drawable = mock<Drawable>()
+        val theme = mock<Theme>()
+        When calling baseResources.getDrawableForDensity(id, 2, theme) doReturn drawable
+        resources.getDrawableForDensity(id, 2, theme) `should equal` drawable
+    }
+
+    @Test
+    fun `Should return Configuration from base resources asking for a configuration`() {
+        val configuration = mock<Configuration>()
+        When calling baseResources.configuration doReturn configuration
+        resources.configuration `should equal` configuration
+    }
+
+    @Test
+    fun `Should return TypedArray from base resources asking for a attributes`() {
+        val attributes = mock<TypedArray>()
+        val attributeSet = mock<AttributeSet>()
+        val attrs = IntArray(3)
+        When calling baseResources.obtainAttributes(attributeSet, attrs) doReturn attributes
+        resources.obtainAttributes(attributeSet, attrs) `should equal` attributes
+    }
+
+    @Test
+    fun `Should return Int from base resources asking for a dimension pixel size`() {
+        When calling baseResources.getDimensionPixelSize(id) doReturn 15
+        resources.getDimensionPixelSize(id) `should equal` 15
+    }
+
+    @Test
+    fun `Should return IntArray from base resources asking for a int array`() {
+        val intArray = IntArray(5)
+        When calling baseResources.getIntArray(id) doReturn intArray
+        resources.getIntArray(id) `should equal` intArray
+    }
+
+    @Test
+    fun `Should call getValue method from base resources asking value by name`() {
+        val outValue = mock<TypedValue>()
+        resources.getValue(id, outValue, true)
+        verify(baseResources).getValue(id, outValue, true)
+    }
+
+    @Test
+    fun `Should call getValue method from base resources asking value by id`() {
+        val outValue = mock<TypedValue>()
+        resources.getValue("id", outValue, true)
+        verify(baseResources).getValue("id", outValue, true)
+    }
+
+    @Test
+    fun `Should return String from base resources asking for a resource package name`() {
+        val packageName = "com.package.name"
+        When calling baseResources.getResourcePackageName(id) doReturn packageName
+        resources.getResourcePackageName(id) `should equal` packageName
+    }
+
+    @Test
+    fun `Should return AssetFileDescriptor from base resources on open raw resource`() {
+        val fileDescriptor = mock<AssetFileDescriptor>()
+        When calling baseResources.openRawResourceFd(id) doReturn fileDescriptor
+        resources.openRawResourceFd(id) `should equal` fileDescriptor
+    }
+
+    @Test
+    fun `Should return Float from base resources asking for a dimension`() {
+        val dimension = 15f
+        When calling baseResources.getDimension(id) doReturn dimension
+        resources.getDimension(id) `should equal` dimension
+    }
+
+    @Test
+    fun `Should return ColorStateList from base resources asking for a color state list`() {
+        val colorStateList = mock<ColorStateList>()
+        When calling baseResources.getColorStateList(id) doReturn colorStateList
+        resources.getColorStateList(id) `should equal` colorStateList
+    }
+
+    @Test
+    fun `Should return ColorStateList from base resources asking for a color state list on API 23+`() {
+        val colorStateList = mock<ColorStateList>()
+        val theme = mock<Theme>()
+        When calling baseResources.getColorStateList(id, theme) doReturn colorStateList
+        resources.getColorStateList(id, theme) `should equal` colorStateList
+    }
+
+    @Test
+    fun `Should return Boolean from base resources asking for a boolean`() {
+        When calling baseResources.getBoolean(id) doReturn true
+        resources.getBoolean(id) `should equal` true
+    }
+
+    @Test
+    fun `Should return Int from base resources asking for a identifier`() {
+        When calling baseResources.getIdentifier("name", "defType", "defPackage") doReturn 5
+        resources.getIdentifier("name", "defType", "defPackage") `should equal` 5
+    }
+
+    @Test
+    fun `Should return Int from base resources asking for a color`() {
+        When calling baseResources.getColor(id) doReturn 15
+        resources.getColor(id) `should equal` 15
+    }
+
+    @Test
+    fun `Should return Int from base resources asking for a color on API 23+`() {
+        val theme = mock<Theme>()
+        When calling baseResources.getColor(id, theme) doReturn 15
+        resources.getColor(id, theme) `should equal` 15
+    }
+
+    @Test
+    fun `Should return InputStream from base resources on open raw resource`() {
+        val inputStream = mock<InputStream>()
+        When calling baseResources.openRawResource(id) doReturn inputStream
+        resources.openRawResource(id) `should equal` inputStream
+    }
+
+    @Test
+    fun `Should return InputStream from base resources on open raw resource with typed value param`() {
+        val inputStream = mock<InputStream>()
+        val typedValue = mock<TypedValue>()
+        When calling baseResources.openRawResource(id, typedValue) doReturn inputStream
+        resources.openRawResource(id, typedValue) `should equal` inputStream
+    }
+
+    @Test
+    fun `Should return Movie from base resources asking for a movie`() {
+        val movie = mock<Movie>()
+        When calling baseResources.getMovie(id) doReturn movie
+        resources.getMovie(id) `should equal` movie
+    }
+
+    @Test
+    fun `Should return Integer from base resources asking for a integer`() {
+        When calling baseResources.getInteger(id) doReturn 7
+        resources.getInteger(id) `should equal` 7
+    }
+
+    @Test
+    fun `Should call parseBundleExtras method from base resources on parse bundle extras`() {
+        val parser = mock<XmlResourceParser>()
+        val bundle = Bundle()
+        resources.parseBundleExtras(parser, bundle)
+        verify(baseResources).parseBundleExtras(parser, bundle)
+    }
+
+    @Test
+    fun `Should return Drawable from base resources asking for a drawable`() {
+        val drawable = mock<Drawable>()
+        When calling baseResources.getDrawable(id) doReturn drawable
+        resources.getDrawable(id) `should equal` drawable
+    }
+
+    @Test
+    fun `Should return Drawable from base resources asking for a drawable on API 21+`() {
+        val drawable = mock<Drawable>()
+        val theme = mock<Theme>()
+        When calling baseResources.getDrawable(id, theme) doReturn drawable
+        resources.getDrawable(id, theme) `should equal` drawable
+    }
+
+    @Test
+    fun `Should return String from base resources asking for a resource type name`() {
+        val typeName = "typeName"
+        When calling baseResources.getResourceTypeName(id) doReturn typeName
+        resources.getResourceTypeName(id) `should equal` typeName
+    }
+
+    @Test
+    fun `Should return XmlResourceParser from base resources asking for a layout`() {
+        val xmlParser = mock<XmlResourceParser>()
+        When calling baseResources.getLayout(id) doReturn xmlParser
+        resources.getLayout(id) `should equal` xmlParser
+    }
+
+    @Test
+    fun `Should return Typeface from base resources asking for a font`() {
+        val typeface = mock<Typeface>()
+        When calling baseResources.getFont(id) doReturn typeface
+        resources.getFont(id) `should equal` typeface
+    }
+
+    @Test
+    fun `Should return XmlResourceParser from base resources asking for a xml`() {
+        val xmlParser = mock<XmlResourceParser>()
+        When calling baseResources.getXml(id) doReturn xmlParser
+        resources.getXml(id) `should equal` xmlParser
+    }
+
+    @Test
+    fun `Should return String from base resources asking for a resource name`() {
+        val resourceName = "resourceName"
+        When calling baseResources.getResourceName(id) doReturn resourceName
+        resources.getResourceName(id) `should equal` resourceName
+    }
+
+    @Test
+    fun `Should call parseBundleExtra method from base resources on parse bundle extra`() {
+        val tagName = "tagName"
+        val attrs = mock<AttributeSet>()
+        val bundle = Bundle()
+        resources.parseBundleExtra(tagName, attrs, bundle)
+        verify(baseResources).parseBundleExtra(tagName, attrs, bundle)
+    }
+
+    @Test
+    fun `Should return Int from base resources asking for a dimension pixel offset`() {
+        When calling baseResources.getDimensionPixelOffset(id) doReturn 15
+        resources.getDimensionPixelOffset(id) `should equal` 15
+    }
+
+    @Test
+    fun `Should call getValueForDensity method from base resources on get value for density`() {
+        val outValue = mock<TypedValue>()
+        resources.getValueForDensity(id, 2, outValue, true)
+        verify(baseResources).getValueForDensity(id, 2, outValue, true)
+    }
+
+    @Test
+    fun `Should return String from base resources asking for a resource entry name`() {
+        val entryName = "entryName"
+        When calling baseResources.getResourceEntryName(id) doReturn entryName
+        resources.getResourceEntryName(id) `should equal` entryName
+    }
+
+    @Test
+    fun `Should return Float from base resources asking for a fraction`() {
+        val fraction = 132f
+        When calling baseResources.getFraction(id, 2, 5) doReturn fraction
+        resources.getFraction(id, 2, 5) `should equal` fraction
     }
 }
